8109d77810bd8cf5b8e17798e7a81f4aeec86c48
Futekov3216 <noodle.hell@gmail.com>
2018-04-17 22:34:56 +0300

    glyph intereactions with carusel
    glyph intereactions with carusel

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-  $('.carousel').carousel();
-  setInterval(function() {
-   $('.carousel').carousel('next');
- }, 2000);
+    let deg = 20;
+    $('.carousel').carousel();
+    setInterval(function() {
+      deg = deg + 20;
+      $('.glyphicon-cog').css({
+        transform: 'rotate('+ deg +'deg)'
+      });
+      $('.carousel').carousel('next');
+   }, 2000);
+
+   $(window).scroll(function(){
+      var $maxScroll=300;
+      var $maxScale=1.3;
+      var $x=$(window).scrollTop()/1000+1;
+      if($(window).scrollTop()>$maxScroll) $x=$maxScale;
+          $('.firstSection h1,h2').css({
+            transform: 'scale('+$x+','+$x+')'
+          });
+
+
+  });
 });
