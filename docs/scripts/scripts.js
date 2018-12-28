24f676a213bea4530aa4e8cb38ff5735b64645a0
Futekov3216 <noodle.hell@gmail.com>
2018-12-28 23:54:15 +0200

    update situation

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -20,4 +20,20 @@ $(document).ready(function(){
 
 
   });
+  var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+let elem = document.getElementsByClassName('now')[0]
+
+if(dd<10) {
+    dd = '0'+dd
+} 
+
+if(mm<10) {
+    mm = '0'+mm
+} 
+
+today = dd + '/' + mm + '/' + yyyy;
+elem.innerHTML = today
 });
